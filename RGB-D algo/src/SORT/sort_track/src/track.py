92df7bb0d4d1e216ac2e2a5285fdefa1e3d8219b
sergesaaybi <sergesaaybi1@hotmail.com>
2021-09-12 19:18:17 +0200

    updated DRL

--- RGB-D algo/src/SORT/sort_track/src/track.py
+++ RGB-D algo/src/SORT/sort_track/src/track.py
@@ -5,6 +5,9 @@ ROS node to track objects using SORT and YOLOv3 (darknet_ros)
 """
 
 
+from threading import currentThread
+
+from pandas.core.frame import DataFrame
 import rospy
 import numpy as np
 from darknet_ros_msgs.msg import BoundingBoxes
@@ -23,14 +26,20 @@ from message_filters import ApproximateTimeSynchronizer, Subscriber
 import message_filters
 import math 
 import pandas as pd
-df_breaches = pd.DataFrame(columns=('Person_Breaching', 'nb_breaches', 'XCoord','YCoord')) #Person_Breaching is a list of persons
-df = pd.DataFrame(columns=('ID','Person','Xmax', 'Xmin', 'Ymax', 'Ymin'))
+df_breaches = pd.DataFrame(columns=('Breach_ID','Person_Breaching', 'nb_breaches', 'XCoord','YCoord')) #Person_Breaching is a list of persons
+df_persons = pd.DataFrame(columns=('PID','Person','Breach_ID'))
+
 id_set = set()
 person_set=set()
+breaches_dict={}
 robot_moving=False
 chosen_row = 0
 nav_x=0
 nav_y =0
+breaches_set=set()
+dic_ID=1
+set2=set([frozenset([1,2]), frozenset([3,4])])
+
 class Person:
 	def __init__(self, id, xmin,xmax,ymin,ymax):
 		self.id = id
@@ -39,37 +48,53 @@ class Person:
 		self.ymin= ymin
 		self.ymax= ymax
 		self.x=((self.xmin+self.xmax)/2)-4
+		#self.x=((self.xmin+self.xmax)/2)
 		self.y=(self.ymin+self.ymax)/2
 		self.euc=0
 		self.breach=False
-		self.mylist = []
 	
-	def breaching (self, other):
+	def breaching (self, other,df):
 		dist_rob=4
+		global dic_ID
 		x=((self.xmin+self.xmax)/2)-4
+		#x=((self.xmin+self.xmax)/2)
 		y=(self.ymin+self.ymax)/2
 		x_other=((other.xmin+other.xmax)/2)-4
+		#x_other=((other.xmin+other.xmax)/2)
 		y_other=(other.ymin+other.ymax)/2
 		euc_distance=math.sqrt(((x_other-x)**2)+((y_other-y)**2))
 		self.euc=euc_distance
-		if (euc_distance<1.5):
+		if (euc_distance<1.5 and self.id != other.id):
 			self.breach=True
 			other.breach=True
-			self.mylist.append(self)
-			self.mylist.append(other) 
-			other.mylist.append(self)
-			other.mylist.append(other)
-			in_first = set(self.mylist)
-			in_second = set(other.mylist)
-			in_second_but_not_in_first = in_second - in_first
-			self.mylist = self.mylist + list(in_second_but_not_in_first)
+			temp1=df.iat[self.id,2]
+			temp2=df.iat[other.id,2]
+			if (temp1 ==0 and temp2==0):
+				temp = set()
+				temp.add(self.id)
+				temp.add(other.id)
+				breaches_dict[dic_ID] = temp
+				df.loc[self.id, 'Breach_ID'] = dic_ID
+				df.loc[other.id, 'Breach_ID'] = dic_ID
+				dic_ID+=1
+			elif (temp1 ==0 and temp2!=0):
+				temp = set()
+				temp.add(self.id)
+				temp.add(other.id)
+				temp_list=temp.union(breaches_dict.get(temp2))
+				breaches_dict[dic_ID] = temp_list
+				# breaches_dict.pop(temp2)
+				df.loc[self.id, 'Breach_ID'] = dic_ID
+				df.loc[other.id, 'Breach_ID'] = dic_ID	
+				dic_ID+=1			
+			else:
+				temp_list=breaches_dict.get(temp1).union(breaches_dict.get(temp2))
+				breaches_dict[temp1] = temp_list
+				#breaches_dict.pop(temp2)
+				df.loc[other.id, 'Breach_ID'] = temp1
 
 
 def get_parameters():
-	"""
-	Gets the necessary parameters from .yaml file
-	Returns tuple
-	"""
 	camera_topic = rospy.get_param("~camera_topic")
 	detection_topic = rospy.get_param("~detection_topic")
 	tracker_topic = rospy.get_param('~tracker_topic')
@@ -80,42 +105,45 @@ def get_parameters():
 
 def checkForBreach(new_person,df):
 	for ind in df.index:
-		#print ('comparing ID: ',new_person.id, 'and ID: ', df['Person'][ind].id)
-		new_person.breaching(df['Person'][ind])
+		new_person.breaching(df['Person'][ind],df)
 	return new_person.breach
 
 def getCoord(breachesList):
+	global df_persons
 	sum_x=0
 	sum_y=0
 	count=len(breachesList)
-	
+	#print (breachesList)
 	for i in breachesList:
-		sum_x+=i.x
-		sum_y+=i.y
+		sum_x+=df_persons.iat[i,1].x
+		sum_y+=df_persons.iat[i,1].y
 	return sum_x/count, sum_y/count
 	
-def gather_breaches(person_set):
+def gather_breaches():
 	global nav_x
 	global nav_y #x and y which are used by the robot for navigation
 	global robot_moving
-	global df_breaches
-	global chosen_row
-	for person in person_set:
-		x,y=getCoord(person.mylist)
-		new_row = {'Person_Breaching':person.mylist, 'nb_breaches': len(person.mylist), 'XCoord':x,'YCoord':y}
-		df_breaches = df_breaches.append(new_row, ignore_index=True)
-	print (df_breaches)
-	row = df_breaches.loc[df_breaches['nb_breaches'].astype(float).idxmax()]
+	global breaches_dict
+	count=0
+	max_key=0
+
+	for key, value in breaches_dict.items():
+		if (count==0):
+			max=len(value)
+			max_key=key
+		elif max<len(value):
+			max=len(value)
+			max_key=key
+			
+	x,y=getCoord(breaches_dict.get(max_key))
 	if (robot_moving==False):
 		robot_moving=True
-		chosen_row = row
-		x_val=row.iloc[2]
-		y_val=row.iloc[3]
-		nav_x=x_val
-		nav_y=y_val
-		return x_val,y_val
+		nav_x=x
+		nav_y=y
+		print('here',nav_x )
+		return nav_x,nav_y
 	else:
-		return 0,0
+		return nav_x,nav_y
 
 #Check if the new coordinates are still close to the initial ones,
 #if not then we need to send new coordintes for the robot as the pedestraisn detected probably moved
@@ -124,18 +152,15 @@ def check_coord(x_val,y_val):
 	if not ((nav_x-0.5<=x_val<=nav_x+0.5) and (nav_y-0.5<=x_val<=nav_y+0.5)): 
 		nav_x=x_val
 		nav_y=y_val
-	#send the new coordinates to the robot
 
 
 def callback_det(data):
-#Set the index to become the ‘ID’ column
-#	df.set_index('ID') #local variable 'df' referenced before assignment
 	global id_set
 	global df
-	global person_set
 	global detections
 	global trackers
 	global track
+	global df_persons
 	breach_bool=False
 	detections = []
 	trackers = []
@@ -144,59 +169,42 @@ def callback_det(data):
 	global robot_moving 
 	computeboxes=ComputeBoxes()
 	computebox=ComputeBox()
-
 	for box in data.bounding_boxes:
 		detections.append(np.array([box.xmin_2d, box.ymin_2d, box.xmax_2d, box.ymax_2d, round(box.probability,2)]))
 		detections = np.array(detections)
 		trackers = tracker.update(detections)
 		trackers = np.array(trackers, dtype=np.int32)
-		# if (trackers.size!=0):
-		# 	computebox.Class=box.Class
-		# 	computebox.id=trackers[0][4]
-		# 	computebox.probability=box.probability
-		# 	computebox.xmin=box.xmin
-		# 	computebox.ymin=box.ymin
-		# 	computebox.xmax=box.xmax
-		# 	computebox.ymax=box.ymax
-		# 	computeboxes.compute_box.append(computebox)	
-		# print('tackers: ',trackers)
-		# print('new ID',trackers[0][4])
+
 		if (trackers.size!=0):
-			# xCol=df.Xmax
-			# yCol=df.Ymax
-			# if not (xCol.between(box.xmax-0.5,box.xmax+0.5).any() and yCol.between(box.ymax-0.25,box.ymax+0.25).any()):
-			if ((trackers[0][4] not in id_set) and (len(id_set) !=0 )): #can we search directly in the df
-				id_set.add(trackers[0][4])
-				new_person= Person(trackers[0][4],box.xmin,box.xmax,box.ymin,box.ymax)
-				person_set.add(new_person)
-				#fill in the object 
-				#check if there is a breach with any of the other elements
-				if (checkForBreach(new_person,df)):
+			new_ID=trackers[0][4]-1
+			if ((new_ID not in id_set)): #can we search directly in the df
+				
+				id_set.add(new_ID)
+				new_person= Person(new_ID,box.xmin,box.xmax,box.ymin,box.ymax)
+				new_row = {'PID':new_ID,'Person':new_person,'Breach_ID':0}  #do it as a map
+				df_persons = df_persons.append(new_row, ignore_index=True)
+				if ((len(id_set) >1 ) and checkForBreach(new_person,df_persons)):
 					breach_bool = True
-					print("breach detected at ID: ",trackers[0][4])
-					for ind in df.index:
-						#print ('comparing ID: ',new_person.id, 'and ID: ', df['Person'][ind].id)
-						print('ID: ',df['Person'][ind].id, 'Breach: ', df['Person'][ind].breach)
-				new_row = {'ID':trackers[0][4],'Person':new_person, 'Xmax':new_person.xmax, 'Xmin':new_person.xmin, 'Ymax':new_person.ymax, 'Ymin':new_person.ymin}
-				df = df.append(new_row, ignore_index=True)
-				print(df)
-			elif ((trackers[0][4] not in id_set) and (len(id_set) ==0 )):
-				id_set.add(trackers[0][4])
-				new_person= Person(trackers[0][4],box.xmin,box.xmax,box.ymin,box.ymax)
-				person_set.add(new_person)
-				new_row = {'ID':trackers[0][4],'Person':new_person, 'Xmax':new_person.xmax, 'Xmin':new_person.xmin, 'Ymax':new_person.ymax, 'Ymin':new_person.ymin}
-				df = df.append(new_row, ignore_index=True)
-				print(df)
+					# print("breach detected at ID: ",new_ID)
+				print(df_persons)
+			
 		detections = []
 		trackers = []
 		computebox=ComputeBox()
+		#print(df_persons)
 	if (breach_bool):
-		x_val,y_val=gather_breaches(person_set)
+		x_val,y_val=gather_breaches()
+		print(df_persons)
+		print(breaches_dict)
 		#we need to make sure if the coordinates are changing or not 
 		check_coord(x_val,y_val)
-
-	# pub_trackers = rospy.Publisher('sort_track', ComputeBoxes,queue_size=10)
-	# pub_trackers.publish(computeboxes)
+		breach_bool=False
+		pub = rospy.Publisher('findObjects', Object, queue_size=1)
+		msg = Object()
+		msg.x= x_val
+		msg.y= y_val
+		msg.z= 0
+		pub.publish(msg)
 
 def publish(trackers,box):
 	computeboxes=ComputeBoxes()
@@ -242,24 +250,10 @@ def main():
 		(camera_topic, detection_topic, tracker_topic, cost_threshold, max_age, min_hits) = get_parameters()
 		tracker = sort.Sort(max_age=max_age, min_hits=min_hits) #create instance of the SORT tracker
 		cost_threshold = cost_threshold
-		#Subscribe to image topic
-		#image_sub = rospy.Subscriber(camera_topic,Image,callback_image)
-		#Subscribe to darknet_ros to get BoundingBoxes from YOLOv3
 		print('going in')
-		#sub_detection = rospy.Subscriber(detection_topic, BoundingBoxes , callback_det)
 		sub_detection = rospy.Subscriber('/darknet_ros_3d/bounding_boxes', BoundingBoxes3d , callback_det)
-		# sub_detection = message_filters.Subscriber(detection_topic, BoundingBoxes)
-		# sub_3Dbbox = message_filters.Subscriber('/darknet_ros_3d/bounding_boxes', BoundingBoxes3d)
-		# ats = ApproximateTimeSynchronizer([sub_detection, sub_3Dbbox], queue_size=5, slop=0.1)
-		# ats.registerCallback(callback_sync)
-
-		#Publish results of object tracking
-		# pub_trackers = rospy.Publisher(tracker_topic, IntList, queue_size=10)
-		# #print(msg) #Testing msg that is published
-		# pub_trackers.publish(msg)
 		rate.sleep()
 		rospy.spin()
-
 
 if __name__ == '__main__':
 	try :
