2de7fa4a325d13af6c35dfcce4370642b138a592
sergesaaybi <sergesaaybi1@hotmail.com>
2022-02-25 00:11:37 +0100

    SAC algorithm

--- robot/build/custom_msgs/CMakeFiles/custom_msgs_generate_messages_nodejs.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/serge/github/robot/devel/share/gennodejs/ros/custom_msgs/msg/Object.js"
-  "CMakeFiles/custom_msgs_generate_messages_nodejs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/custom_msgs_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
